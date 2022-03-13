5416a0ababa40c29b87e86fb9972eb583988c039
grezlp <101464457+grezlp@users.noreply.github.com>
2022-03-13 15:39:55 +0100

    Add files via upload (#3)

---
+++ ctverec.py
@@ -0,0 +1,24 @@
+print("Obvod čtverce se stranou 356 cm je ",356*4," cm")
+print("Obsah čtverce se stranou 356 cm je ",356**2," cm2")
+
+print("Obvod čtverce se stranou 356 cm je ",123*4," cm")
+print("Obsah čtverce se stranou 356 cm je ",123**2," cm2")
+
+strana = 123
+print("Obvod čtverce se stranou", strana, "je", 4 * strana, "cm")
+print("Obsah čtverce se stranou", strana, "je",  strana ** 2, "cm2")
+
+print("Obvod kruhu s poloměrem", strana, "je", 2*3.1415926*strana, "cm")
+print("Obsah kruhu s poloměrem", strana, "je", 3.1415926*strana**2, "cm2")
+
+#takhle se píše komentář, noice
+#strana = float(input("Zadej stranu čtverce v centimetrech: "))
+#print("Obvod čtverce se stranou", strana, "je", 4 * strana, "cm")
+#print("Obsah čtverce se stranou", strana, "je",  strana ** 2, "cm2")
+
+pravda = 1<3
+print(pravda)
+nepravda = 1==3
+print(nepravda)
+print(True)
+print(False)
