6f739c4de86ebdaf82786e2900e240161140156b
grezlp <101464457+grezlp@users.noreply.github.com>
2022-03-13 15:48:55 +0100

    Add files via upload

---
+++ grafika.py
@@ -0,0 +1,13 @@
+import pyglet
+window = pyglet.window.Window()
+
+def zpracuj_text(text):
+    print(text)
+
+window.push_handlers(on_text=zpracuj_text)
+
+pyglet.app.run()
+print('Hotovo!')
+
+vypis = print
+vypis("Ahoj světe!")
