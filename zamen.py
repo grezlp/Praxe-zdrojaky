93c98e748705877e81108e6149b77acabcf34e0d
grezlp <101464457+grezlp@users.noreply.github.com>
2022-03-13 15:37:40 +0100

    Delete zamen.py

--- zamen.py
+++
@@ -1,9 +0,0 @@
-slovo = input('Slovo: ')
-pozice = int(input('Které písmeno zaměnit (od nuly)? '))
-novy_znak = input('Nové písmeno: ')
-
-zacatek_slova = slovo[:pozice]
-konec_slova = slovo[pozice + 1:]
-nove_slovo = zacatek_slova + novy_znak + konec_slova
-
-print(nove_slovo)
