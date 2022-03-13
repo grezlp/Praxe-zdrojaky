aa1a0d87dbe403f1c16640da413cb2d5ab7508a8
grezlp <101464457+grezlp@users.noreply.github.com>
2022-03-13 15:37:22 +0100

    Delete sablony2.py

--- sablony2.py
+++
@@ -1,7 +0,0 @@
-sablona = 'Ahoj {jmeno}! Tvoje číslo je {cislo}.'
-print(sablona.format(cislo=7, jmeno='Hynku'))
-print(sablona.format(cislo=42, jmeno='Viléme'))
-print(sablona.format(cislo=3, jmeno='Jarmilo'))
-
-vypis = '{} krát {} je {}'.format(3, 4, 3*4)
-print(vypis)
